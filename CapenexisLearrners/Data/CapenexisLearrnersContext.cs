9cecbe3fa72bdcbe8223f507490505db2ac1b91e
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-01-09 08:44:39 +0200

    Add project files.

---
+++ CapenexisLearrners/Data/CapenexisLearrnersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CapenexisLearrners.Models;
+
+namespace CapenexisLearrners.Data
+{
+    public class CapenexisLearrnersContext : DbContext
+    {
+        public CapenexisLearrnersContext (DbContextOptions<CapenexisLearrnersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CapenexisLearrners.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ CapenexisLearrners/Models/Learner.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearrners.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? FirstName{ get; set; }
+    public string?LastName { get; set; }
+    public string? NationalIdentityNumber { get; set; }
+    public string? Course { get; set; }
+    public decimal Price { get; set; }
+}
